360c6b0a2f5b7a8caaf03d72c320cbb74be59396
Valquiria Perea <94647582+valpereag@users.noreply.github.com>
2022-02-03 19:34:47 -0600

    Add files via upload

---
+++ Pseudocode Lab.py
@@ -0,0 +1,38 @@
+"""
+Program Name: CollegeAdmission.py
+Function: This program determines if a student will be admitted or rejected.
+Input:  Interactive
+Output: Accept or Reject
+"""
+testScoreString = input("testScoreString")
+classRankString = input("classRankString")
+
+print (type(testScoreString))
+print (type(classRankString))
+
+testScore = int(testScoreString)
+classRank = int(classRankString)
+
+print (type(testScore))
+print (type(classRank))# Get input and convert to correct data type for testScore and classRank
+# Test using admission requirements and print Accept or Reject
+if testScore >= 90:
+    if classRank >= 25:
+        print("Accept")
+    else:
+        print("Reject")
+else:
+    if testScore >= 80:
+        if classRank >= 50:
+            print("Accept")
+        else:
+            print("Reject")
+    else:
+        if testScore >= 70:
+            if classRank >= 75:
+                print("Accept")
+            else:
+                print("Reject")
+        else:
+            print("Reject")
+
